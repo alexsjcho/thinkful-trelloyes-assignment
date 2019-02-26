bb61ea6dee8b453f7fe34724f5ef723cb6556bc7
Alex Cho <alexsjcho@gmail.com>
2019-02-26 13:55:13 -0800

    added button features to this app

--- src/test/List.test.js
+++ src/test/List.test.js
@@ -1,21 +1,9 @@
-import React from "react";
-import Card from "./Card";
-import "./List.css";
 
-export default function List(props) {
-  return (
-    <section className="List">
-      <header className="List-header">
-        <h2>{props.header}</h2>
-      </header>
-      <div className="List-cards">
-        {props.cards.map(card => (
-          <Card key={card.id} title={card.title} content={card.content} />
-        ))}
-        <button type="button" className="List-add-button">
-          + Add Random Card
-        </button>
-      </div>
-    </section>
-  );
-}
+
+it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Messages name="Messages" unread={4}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+    });
+    
